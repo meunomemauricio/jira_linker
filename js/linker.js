db449e2385b05cde7d2d8a7ab999a6e520c6cc5f
Mauricio Freitas <meunomemauricio@users.noreply.github.com>
2017-08-26 23:09:16 -0300

    Only substitute text nodes, leaving elements as is

--- js/linker.js
+++ js/linker.js
@@ -3,7 +3,8 @@
  */
 
 // Match Comments and Issue Title
-const SELECTORS = 'td.comment-body,span.js-issue-title';
+const COMMENTS_SELECTOR = 'td.comment-body>p';
+const TITLE_SELECTOR = 'span.js-issue-title';
 
 
 /**
@@ -13,9 +14,28 @@ function substituteIDs(url, projectIds) {
   let linkHTML = `<a target="_blank" href="${url}/$&">$&</a>`;
   projectIds.forEach(function (pId){
     let pattern = new RegExp(`${pId}-\\d+`, 'g');
-    let matches = document.querySelectorAll(SELECTORS);
-    matches.forEach(function(match) {
+    let title = document.querySelectorAll(TITLE_SELECTOR);
+    title.forEach(function(match) {
       match.innerHTML = match.innerHTML.replace(pattern, linkHTML);
+    });
+
+    let comments = document.querySelectorAll(COMMENTS_SELECTOR);
+    comments.forEach(function(match) {
+      match.childNodes.forEach(function(textNode) {
+        // Only substitute Text Nodes
+        if (textNode.nodeType === 3) {
+          // Create a temporary div element to render the new HTML link as proper nodes
+          let temp = document.createElement('div');
+          temp.innerHTML = textNode.data.replace(pattern, linkHTML);
+
+          // Insert the new nodes on the parent and remove the original text node
+          while (temp.firstChild) {
+            textNode.parentNode.insertBefore(temp.firstChild, textNode);
+          }
+          textNode.parentNode.removeChild(textNode);
+        }
+      });
+
     });
   });
 }
